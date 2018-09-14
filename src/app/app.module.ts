fb64b9ecfbefb03e9beb84649ca3cdb3265a0825
Yamkela Venfolo <yamkela.venfolo@SAMAC137.local>
2018-09-15 01:44:37 +0200

    first atempt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchMovieComponent } from './search-movie/search-movie.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { MovieComponent } from './movie/movie.component';
 
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent
+  },
+  {
+    path: 'SearchMovie/:id',
+    component: SearchMovieComponent
+  },
+  {
+    path: 'movieList',
+    component: MovieListComponent
+  },
+  ,
+  {
+    path: 'Movie',
+    component: MovieComponent
+  }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchMovieComponent,
+    MovieListComponent,
+    MovieComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: AppComponent},
+      { path: 'SearchMovie', component: SearchMovieComponent},
+      { path: 'movieList', component: MovieListComponent},
+      { path: 'Movie', component: MovieComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
