7ffdfdb91ffe3720f9ee244fc88030f9d865eb0f
Yamkela Venfolo <yamkela.venfolo@SAMAC137.local>
2018-09-20 12:33:24 +0200

    characters now working

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -8,35 +8,12 @@ import { HttpClient } from '@angular/common/http';
 export class DataService {
   public Movies$: Object;
   public CharacterName$: String[];
-  public Title$: String;
-  public ReleaseDate$: String;
-  public OpeningCrawl$: String;
-  public Produced$: String;
-  public Directed$: String;
-
   constructor(private http: HttpClient) { }
 
   public Movies() {
     return this.http.get('https://swapi.co/api/films/');
   }
-
   public CharactersName(url: any) {
     return this.http.get(url);
-  }
-  public getSelectedMovie(MovieName: any) {
-    if (!MovieName) {
-      this.Movies().subscribe(
-        data => {
-          this.Movies$ = data['results'];
-
-          if (this.Movies$['title'] === MovieName) {
-              this.ReleaseDate$ = this.Movies$['release_date'];
-              this.OpeningCrawl$ = this.Movies$['opening_crawl'];
-              this.Title$ = this.Movies$['title'];
-              this.Produced$ = this.Movies$['producer'];
-              this.Directed$ = this.Movies$['director'];
-          }
-        });
-    }
   }
 }
--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -11,13 +11,27 @@ import { Observable } from 'rxjs';
 export class MovieListComponent implements OnInit {
 
   Movies$: Object;
+  Characters$: String[];
 
-  constructor(private data: DataService) {}
+  constructor(private data: DataService) { }
 
   ngOnInit() {
     this.data.Movies().subscribe(
-        data => { this.Movies$ = data['results'];
-      }
-    );
+      data => {
+        this.Movies$ = data['results'];
+        const characters = [];
+
+        /// Reading the characters from the API
+        for (const character in this.Movies$[0]['characters']) {
+          if (character in this.Movies$[0]['characters']) {
+            const response = fetch(this.Movies$[0]['characters'][character]).then((Pr) => {
+              const e = Pr.json().then(function (readCharacterdetails) {
+                characters.push(readCharacterdetails['name']);
+              });
+            });
+          }
+        }
+        this.Characters$ = characters ;
+      });
   }
 }
